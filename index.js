d220b962bfc50aee3cd1ca5ef5e1905e30e23f07
GSeal <sealgp1@student.op.ac.nz>
2021-10-27 11:34:55 +1300

    create and incorporate template

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
 const express = require('express')
 const app = express()
-const port = process.env.port || 3000
-
-// app.get('/', (req, res) => res.send('index'))
-
+const port = process.env.PORT || 3000
 const path = require('path');
-app.get('/', (req, res) => res.render('index'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.set("view engine","ejs");
-
 app.set('views', path.join(__dirname, 'views'));
-
-
 app.use(express.static(path.join(__dirname, 'assets')))
 
-
-// app.get('/', (req, res) => res.send('index'))
-
-
-// app.get('/contact', (req, res) => {
-//     res.send("Contact page")
-// })
+const expressLayouts = require('express-ejs-layouts')
+app.use(expressLayouts)
 
 
+app.get('/', (req, res) => res.render('index'))
 
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
