a502dd2e98f6962913bb04d4cb412d4cee40c05e
GSeal <sealgp1@student.op.ac.nz>
2021-10-27 12:11:51 +1300

    modified(views/layout.ejs): update layout template

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const path = require('path');
+
 
 app.set("view engine","ejs");
 app.set('views', path.join(__dirname, 'views'));
